1ff25f0e9d82e3c7639703bc821cc07231dfa83f
Jer Warren <jerwarren@gmail.com>
2022-08-23 14:16:20 -0700

    reverse logic on update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', domReady);
 const meter = document.querySelector(".meter span");
-const seconds = 10;
+const seconds = 11;
 
 var count = 1;
 function domReady(){
@@ -9,21 +9,17 @@ function domReady(){
 }
 
 function oneSecond(){
-    console.log(count);
-
     meter.style="width:"+count*seconds+"%";
     
-    if (count >= seconds){
-        redirect();
-    } else {
+    if (count <= seconds){
         count++;
-    setTimeout(oneSecond, 1000);
+        setTimeout(oneSecond, 1000);
+    } else {
+        redirect();
     }
-    
-    
 }
 
 function redirect(){
-console.log("redirecting");
-window.location.href="https://www.northernquest.com";
+    console.log("redirecting");
+    window.location.href="https://www.northernquest.com";
 }
